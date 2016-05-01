640328d1047e2714565f489ef7a9bdf246647b12
Joel Francisco <joel.francisco@zenn.pt>
2016-05-01 04:39:08 +0200

    pfc

---
+++ db/access.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Capability definitions for this module.
+ *
+ * @package local_pfc
+ * @copyright 2016, Duarte Mateus <2120189@my.ipleiria.pt>, Joel Francisco <2121000@my.ipleiria.pt>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$capabilities = array(
+    'local/pfc:executesynchronization' => array(
+        'riskbitmask'  => RISK_DATALOSS,
+        'captype'      => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes'   => array(
+            'student'        => CAP_PROHIBIT,
+            'teacher'        => CAP_PROHIBIT,
+            'editingteacher' => CAP_PROHIBIT,
+            'manager'          => CAP_ALLOW
+        )
+    ),
+);
---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information
+ *
+ * @package local_pfc
+ * @copyright 2016, Duarte Mateus <2120189@my.ipleiria.pt>, Joel Francisco <2121000@my.ipleiria.pt>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_pfc';    // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_ALPHA;    // Declares the maturity level of this plugin version (MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE)
+$plugin->release = 'v0.0-a1';    // Human readable version name that should help to identify each release of the plugin
+$plugin->requires = 2014051200;    // Requires this Moodle version.
+$plugin->version  = 2016050100;    // The current module version (Date: YYYYMMDDXX).
