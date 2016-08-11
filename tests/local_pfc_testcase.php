7ad7e679a4575034c8f670dbce68d4c0f38638fa
LuoSKraD <slayerpro@gmail.com>
2016-08-11 19:54:05 +0100

    first test case

--- tests/local_pfc_testcase.php
+++ tests/local_pfc_testcase.php
@@ -20,18 +20,102 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+class local_pfc_webServiceConnector
+{
+
+    /**
+     * @var \local_pfc\api\calendar_api
+     */
+    private $calendar_api;
+
+    /**
+     * @var \local_pfc\api\evaluation_api
+     */
+    private $evaluation_api;
+
+    /**
+     * @var \local_pfc\api\evaluation_type_api
+     */
+    private $evaluation_type_api;
+
+    /**
+     * local_pfc_api_interface constructor.
+     */
+    public function __construct()
+    {
+        $api_client = new \local_pfc\api_client();
+        $this->calendar_api = new \local_pfc\api\calendar_api($api_client);
+        $this->evaluation_api = new \local_pfc\api\evaluation_api($api_client);
+        $this->evaluation_type_api = new \local_pfc\api\evaluation_type_api($api_client);
+    }
+    /**
+     * @return \local_pfc\models\calendar[]
+     */
+    function get_calendars() {
+        return $this->calendar_api->get_calendars();
+    }
+
+    /**
+     * @return \local_pfc\models\evaluation[]
+     */
+    function get_evaluations() {
+        return $this->evaluation_api->get_evaluations();
+    }
+
+    /**
+     * @return \local_pfc\models\evaluation_type[]
+     */
+    function get_evaluation_types() {
+        return $this->evaluation_type_api->get_evaluation_types();
+    }
+}
+
+
+class test_pfc_webservice extends advanced_testcase{
+
+
+    public function test_get_calendars() {
+        global $CFG;
+        require_once($CFG->dirroot . '/local/pfc/lib.php');
+        $WS= new local_pfc_webServiceConnector();
+        $this->assertNotEmpty($WS->get_calendars());
+    }
+
+
+    public function test_get_evaluations() {
+        global $CFG;
+        require_once($CFG->dirroot . '/local/pfc/lib.php');
+        $WS = new local_pfc_webServiceConnector();
+        $this->assertNotEmpty($WS->get_evaluations());
+    }
+
+    public function test_get_evaluation_types() {
+        global $CFG;
+        require_once($CFG->dirroot . '/local/pfc/lib.php');
+        $WS = new local_pfc_webServiceConnector();
+        $this->assertNotEmpty($WS->get_evaluation_types());
+    }
+
+
+    public function test_webService_header(){
+
+    }
+
+}
+
+
 
 class local_pfc_testcase extends advanced_testcase {
-    protected $testassertexecuted = false;
-
-    protected function setUp() {
-        parent::setUp();
-        if ($this->getName() === 'test_setup_assert') {
-            $this->assertTrue(true);
-            $this->testassertexecuted = true;
-            return;
-        }
-    }
+//    protected $testassertexecuted = false;
+//
+//    protected function setUp() {
+//        parent::setUp();
+//        if ($this->getName() === 'test_setup_assert') {
+//            $this->assertTrue(true);
+//            $this->testassertexecuted = true;
+//            return;
+//        }
+//    }
 
     /**
      * Tests that bootstrapping has occurred correctly
@@ -44,4 +128,11 @@ class local_pfc_testcase extends advanced_testcase {
         $this->assertEquals($CFG->prefix, $CFG->phpunit_prefix);
     }
 
+
+//    public function test_webservice() {
+//        $this->assertNotEmpty();
+//    }
+
 }
+
+
